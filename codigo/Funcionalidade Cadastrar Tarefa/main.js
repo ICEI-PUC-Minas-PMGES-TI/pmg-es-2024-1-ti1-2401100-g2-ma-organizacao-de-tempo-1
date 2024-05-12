dd98ab9e59b719a85fc6acd822cda6da0449cef7
Gobiraa <164088812+Gobiraa@users.noreply.github.com>
2024-05-12 18:58:19 -0300

    Add files via upload
    Adicionado HTML, CSS e JS do artefato de cadastro de tarefas.

---
+++ codigo/Funcionalidade Cadastrar Tarefa/main.js
@@ -0,0 +1,9 @@
+function fechar() {
+    
+}
+
+function criar() {
+    var tarefa = document.getElementById('tarefa').value;
+    var horario = document.getElementById('horario').value;
+    alert('Tarefa "' + tarefa + '" criada para o horário ' + horario);
+}
