21a59a72d022e0dd7f62fa6bfbe6a7f1532f3ac7
Gobiraa <jpgobira2@gmail.com>
2024-06-02 20:24:43 -0300

    responsividade
    adicionada responsividade no site

--- codigo/assets/scripts/coins.js
+++ codigo/assets/scripts/coins.js
@@ -1,9 +1,8 @@
 let cronoCoins = JSON.parse(localStorage.getItem('cronoCoins')) || 100;
 
     function atualizarSaldo() {
-      // Atualizando o saldo de CronoCoins na div
       document.getElementById('saldocoins').innerText = `${cronoCoins}`;
-      // Armazenando o saldo de CronoCoins no localStorage
+      
       localStorage.setItem('cronoCoins', JSON.stringify(cronoCoins));
     }
 
